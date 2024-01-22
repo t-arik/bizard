adb16bfa971d82fe4be2a3a48b3a4f50a57e9b6b
Tarik <tarik.badawy@studium.uni-hamburg.de>
2024-01-22 23:29:35 +0100

    feat: add tailwindcss

--- controller/templates.ex
+++ controller/templates.ex
@@ -4,23 +4,35 @@ defmodule Bizard.Template do
   """
   require EEx
 
-  EEx.function_from_file(
-    :def,
-    :index,
-    "views/index.heex",
-    [:content]
-  )
+  def index(content) do
+    EEx.eval_file("views/index.heex", content: content)
+  end
 
-  EEx.function_from_file(
-    :def,
-    :register,
-    "views/register.heex"
-  )
+  def register() do
+    EEx.eval_file("views/register.heex")
+  end
 
-  EEx.function_from_file(
-    :def,
-    :game,
-    "views/game.heex",
-    [:game, :player]
-  )
+  def game(game, player) do
+    EEx.eval_file("views/game.heex", game: game, player: player)
+  end
+
+  # EEx.function_from_file(
+  #   :def,
+  #   :index,
+  #   "views/index.heex",
+  #   [:content]
+  # )
+
+  # EEx.function_from_file(
+  #   :def,
+  #   :register,
+  #   "views/register.heex"
+  # )
+
+  # EEx.function_from_file(
+  #   :def,
+  #   :game,
+  #   "views/game.heex",
+  #   [:game, :player]
+  # )
 end
